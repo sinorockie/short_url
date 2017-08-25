ea3dbdf40c53f5d58fc56b042e269eadb9735877
Lei Shi <sinorockie@users.noreply.github.com>
2017-08-26 02:01:56 +0800

    Fix deprecated

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var app = express();
 app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({extended: true}));
 
 require('./routes')(app);
 app.listen(1754);
